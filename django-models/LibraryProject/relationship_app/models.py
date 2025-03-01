32a1e641a0e44bd8b89f7584be076c7b3bf47afb
MarouaneOuchala <marouaneouchalla@gmail.com>
2025-03-01 20:55:55 +0000

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,3 +12,17 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __str__(self):
+        return self.name
