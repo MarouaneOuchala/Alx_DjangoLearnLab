186be2aa8e0ae124f9201efe9574a831a8792206
MarouaneOuchala <marouaneouchalla@gmail.com>
2025-03-01 21:12:17 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,4 +3,4 @@ from .models import Book  # Ensure you have the Book model
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/book_list.html', {'books': books})  # Ensure the correct template name
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Ensure the correct template name
