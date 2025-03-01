67ae295cff5740057ce58e5b1787168cbd036ed4
MarouaneOuchala <marouaneouchalla@gmail.com>
2025-03-01 19:51:19 +0000

    Update models.py

--- django-models/models.py
+++ django-models/models.py
@@ -1,40 +1,7 @@
-import os
-import django
+rom django.db import models
 
-# Set up Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
-django.setup()
+class Author(models.Model):
+    name = models.CharField(max_length=255)
 
-from django.db import Author, Book, Library, Librarian
-
-# Query 1: Get all books by a specific author
-def get_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        books = author.books.all()  # Using related_name in ForeignKey
-        return [book.title for book in books]
-    except Author.DoesNotExist:
-        return f"Author '{author_name}' not found."
-
-# Query 2: List all books in a library
-def get_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        books = library.books.all()
-        return [book.title for book in books]
-    except Library.DoesNotExist:
-        return f"Library '{library_name}' not found."
-
-# Query 3: Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian.name if library.librarian else "No librarian assigned."
-    except Library.DoesNotExist:
-        return f"Library '{library_name}' not found."
-
-# Example Usage
-if __name__ == "__main__":
-    print(get_books_by_author("J.K. Rowling"))
-    print(get_books_in_library("City Library"))
-    print(get_librarian_for_library("City Library"))
+    def __str__(self):
+        return self.name
