a76156a44f039320c90b336273678782f41a29a3
MarouaneOuchala <marouaneouchalla@gmail.com>
2025-03-23 22:15:33 +0000

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -1,10 +1,11 @@
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',  # Django REST Framework
-    'api',  # Application API
+    # ...existing apps...
+    "rest_framework",
+    "api",
 ]
+
+["rest_framework.authtoken"]
+
+["rest_framework.authentication.TokenAuthentication"]
+
+["rest_framework.permissions.IsAuthenticated"]
