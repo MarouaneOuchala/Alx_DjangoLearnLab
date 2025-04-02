5f6cb69a8e418974aa694957de2a295e7e3dc6ed
MarouaneOuchala <marouaneouchalla@gmail.com>
2025-04-02 20:39:16 +0000

    zdz

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -20,4 +20,4 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-["post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"]
+"post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
